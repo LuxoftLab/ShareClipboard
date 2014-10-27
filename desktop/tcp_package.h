fa34d549b3b040839e23755292f9c1f9706f06e5
Asal Mirzaeva <asalle.kim@gmail.com>
2014-10-27 13:13:03 +0200

    send password and login to join room

--- desktop/tcp_package.h
+++ desktop/tcp_package.h
@@ -1,10 +1,46 @@
 #ifndef TCP_PACKAGE_H
 #define TCP_PACKAGE_H
 
-class TcpPackage
-{
-public:
-    TcpPackage();
+#ifndef TCPPACKAGE_H
+#define TCPPACKAGE_H
+
+#include <QDataStream>
+
+enum pckg_t{TEXT, PASS, LOGIN, RAW};
+
+union Data{
+    QString* strData;
+    QString* pass;
+    char* rawData;
 };
+
+struct TcpPackageHeader{
+
+    pckg_t type;
+    int length;
+    TcpPackageHeader(char t, int l);
+    TcpPackageHeader();
+};
+
+class TcpPackage{
+    friend QDataStream& operator <<(QDataStream&, const TcpPackage&);
+    friend const QDataStream& operator >>(const QDataStream&, TcpPackage&);
+public:
+    TcpPackage(TcpPackageHeader, Data);
+    TcpPackage();
+
+
+    const TcpPackageHeader* getHeader() const;
+    const Data* getData() const;
+
+private:
+    TcpPackageHeader header;
+    Data interior;
+
+};
+
+
+#endif // TCPPACKAGE_H
+
 
 #endif // TCP_PACKAGE_H
