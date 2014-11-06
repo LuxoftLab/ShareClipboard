3b93b5ebf06fa9b7acf6c458889cf89d5aab8113
vikmaksimenko <vikmaksimenko@hotmail.com>
2014-11-06 09:02:35 +0200

    Added some tests before submit

--- desktop/gui/createroomdialog.cpp
+++ desktop/gui/createroomdialog.cpp
@@ -1,6 +1,7 @@
 #include "createroomdialog.h"
 #include "ui_createroomdialog.h"
 #include "mainwindow.h"
+#include <QMessageBox>
 
 CreateRoomDialog::CreateRoomDialog(QWidget *parent) :
     QDialog(parent),
@@ -16,6 +17,32 @@ CreateRoomDialog::~CreateRoomDialog()
 
 void CreateRoomDialog::accept()
 {
+    if(ui->lineEdit->text().isEmpty())
+    {
+        QMessageBox messageBox;
+        messageBox.setText("Введите имя комнаты!");
+        messageBox.setIcon(QMessageBox::Warning);
+        messageBox.exec();
+        return;
+    }
+
+    if(ui->lineEdit_2->text().isEmpty())
+    {
+        QMessageBox messageBox;
+        messageBox.setText("Введите пароль!");
+        messageBox.setIcon(QMessageBox::Warning);
+        messageBox.exec();
+        return;
+    }
+
+    if(ui->lineEdit_2->text().compare(ui->lineEdit_3->text()) != 0)
+    {
+        QMessageBox messageBox;
+        messageBox.setText("Пароли не совпадают");
+        messageBox.setIcon(QMessageBox::Warning);
+        messageBox.exec();
+        return;
+    }
     emit (createRoom(ui->lineEdit->text(), ui->lineEdit_2->text()));
     close();
 }
