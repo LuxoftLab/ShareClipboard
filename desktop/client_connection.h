2c414010b133561a1b200fb96489c1880fe46b9b
Asal Mirzaeva <asalle.kim@gmail.com>
2015-02-11 14:34:20 +0200

    merge fix

--- desktop/client_connection.h
+++ desktop/client_connection.h
@@ -16,14 +16,12 @@ public:
     void removeMember(QHostAddress addr);
     QString getLogin();
     void sendText(QString text);
+    void makePass(QDataStream&);
+    QHostAddress makeHostAdress(char*);
 public slots:
     void onData();
 signals:
     void verifyPass(QString pass, ClientConnection * const);
-    void makeMember(QDataStream&);
-    void makePass(QDataStream&);
-    QHostAddress makeHostAdress(char*);
-
 };
 
 #endif // CLIENT_CONNECTION_H
